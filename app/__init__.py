1073c7b5df405c1a0b9dbe02510dc330e0838604
Dennis Fazio <fazioferreira@gmail.com>
2020-02-14 07:32:02 -0300

    Tabelas Criadas

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+## engine = sa.create_engine('mssql+pyodbc://DESKTOP-FQPDNGK\SQLEXPRESS/database')
+## engine = app.create_engine('mssql+pyodbc://user:password@server/database')
+app.config['SQLALCHEMY_DATABAE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
 
-
-
+from app.controllers import default
